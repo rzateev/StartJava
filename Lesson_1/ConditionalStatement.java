5c5ba4ea4390a407f00d22a32bfe19c96188ce6d
Roman Zateev <rzateev@gmail.com>
2021-11-21 22:07:22 +0700

    Code formatted

--- Lesson_1/ConditionalStatement.java
+++ Lesson_1/ConditionalStatement.java
@@ -1,36 +1,34 @@
-public class ConditionalStatement {	
+public class ConditionalStatement { 
     public static void main(String[] args) {
         int age = 15;
-        boolean isMan = true;
-        double height = 1.73;
-        char firstCharOfName = 'R';
 
-        if (age>10) {
-        	System.out.println("Age ="+age);
+        if (age > 10) {
+            System.out.println("Age =" + age);
         }
 
+        boolean isMan = true;
         if (isMan) {
-        	System.out.println("I am a man");
+            System.out.println("I am a man");
         }
 
         if (!isMan) {
-        	System.out.println("I am not a man");
+            System.out.println("I am not a man");
         }
 
-        if (height<1.8) {
-        	System.out.println("My height is "+height);
-        }
-        else
-        {
-        	System.out.println("My height >1.8");
-        }
-
-        if (firstCharOfName =='M') {
-        	System.out.println("My first char of name is M");
-        } else if (firstCharOfName =='I') {
-        	System.out.println("My first char of name is I");
+        double height = 1.73;
+        if (height < 1.8) {
+            System.out.println("My height is " + height);
         } else {
-        	System.out.println("My first char of name is "+firstCharOfName);
+            System.out.println("My height >1.8");
+        }
+
+        char firstCharOfName = 'R';
+        if (firstCharOfName =='M') {
+            System.out.println("My first char of name is M");
+        } else if (firstCharOfName =='I') {
+            System.out.println("My first char of name is I");
+        } else {
+            System.out.println("My first char of name is " + firstCharOfName);
         }
     }
 }
--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,21 +1,20 @@
-public class Variable {	
+public class Variable { 
     public static void main(String[] args) {
-         byte cpuCount = 1;
-         short diskCount = 2;
-         int cpuClock = 4700;         
-         long diskSize = 277075537;
-         float memorySize = 16;
-         double versionOfWindows = 11;
-         char firstCharOfName = 'R';
-         boolean haveSdd = true;
+        byte cpuCount = 1;
+        short diskCount = 2;
+        int cpuClock = 4700;         
+        long diskSize = 277075537;
+        float memorySize = 16;
+        double versionOfWindows = 11;
+        char firstCharOfName = 'R';
+        boolean haveSdd = true;
 
-        System.out.println("CPU count="+cpuCount);
-        System.out.println("Disk count="+diskCount);
-        System.out.println("Disk size="+diskSize);
-        System.out.println("Memory size="+memorySize);
-        System.out.println("Windows version="+versionOfWindows);
-        System.out.println("First Char Of Name="+firstCharOfName);
-        System.out.println("Have SDD="+haveSdd);
-
+        System.out.println("CPU count=" + cpuCount);
+        System.out.println("Disk count=" + diskCount);
+        System.out.println("Disk size=" + diskSize);
+        System.out.println("Memory size=" + memorySize);
+        System.out.println("Windows version=" + versionOfWindows);
+        System.out.println("First Char Of Name=" + firstCharOfName);
+        System.out.println("Have SDD=" + haveSdd);
     }
 }
