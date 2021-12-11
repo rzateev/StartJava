0644210c2cff38b0daa082763a187666b35275d8
Roman Zateev <rzateev@gmail.com>
2021-12-12 00:53:22 +0700

    Lession 2 new Wolf

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,30 +1,75 @@
 public class Wolf {	
-    public String name;
-    public Integer age;
-    public Integer weight;
-    public String sex;
-    public String color;
+    private String name;
+    private Integer age;
+    private Integer weight;
+    private String sex;
+    private String color;
 
-    void move() {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        if (age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+           this.age = age;
+        }
+    }
+
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer  weight) {
+        this.weight = weight;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void move() {
         System.out.println("moving");
     } 
 
-    void run() {
+    public void run() {
         System.out.println("running");
     } 
 
-    void sit() {
+    public void sit() {
         System.out.println("sitting");
     }
 
-    void hunt() {
+    public void hunt() {
         System.out.println("hunting");
     }
 
-    void howl() {
+    public void howl() {
         System.out.println("howling");
     }
 
-    
+
 
 }
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,17 +1,17 @@
 public class WolfTest {	
     public static void main(String[] args) {
         Wolf wolf1 = new Wolf();
-        wolf1.name = "Big wolf";
-        wolf1.age = 10;
-        wolf1.weight = 35;
-        wolf1.sex = "man";
-        wolf1.color = "black";
+        wolf1.setName("Big wolf");
+        wolf1.setAge(10);
+        wolf1.setWeight(35);
+        wolf1.setSex("man");
+        wolf1.setColor("black");
 
-        System.out.println(wolf1.name);
-        System.out.println(wolf1.age);
-        System.out.println(wolf1.weight);
-        System.out.println(wolf1.sex);
-        System.out.println(wolf1.color);
+        System.out.println(wolf1.getName());
+        System.out.println(wolf1.getAge());
+        System.out.println(wolf1.getWeight());
+        System.out.println(wolf1.getSex());
+        System.out.println(wolf1.getColor());
         
         wolf1.move();
         wolf1.run();
